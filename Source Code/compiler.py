7e40b938f8d28e903d9dbe39b373b1ea1e34f55d
Joseph Roddis <joeroddis@hotmail.co.uk>
2020-01-21 13:14:46 +0000

    Update

--- Source Code/compiler.py
+++ Source Code/compiler.py
@@ -1,39 +1,49 @@
+#Imports
 import sys
 from PySide2 import QtCore, QtGui
 from PySide2.QtCore import Qt, Slot
 from PySide2.QtGui import QPainter
+#Import various QWidgets
 from PySide2.QtWidgets import (QAction, QApplication, QHeaderView, QHBoxLayout, QLabel, QLineEdit,
                                QMainWindow, QPushButton, QTableWidget, QTableWidgetItem,
                                QVBoxLayout, QWidget, QFileDialog, QDialog, QTextEdit)
 from PySide2.QtCharts import QtCharts
-
+#Import hack language definitions
 from hackdefinitions import comp, dest, jump, symbols
 import re
+import gc
 
+#Set global variables for filepaths
 openpath = None
 savepath = None
+#Used to store output for saving file in MainWindow class
 opfile   = []
 
+#Widget Class
 class Widget(QWidget):
     def __init__(self):
         QWidget.__init__(self)
         self.items = 0
 
-        # Example data
+        # Initialise dictionaries
         self._data = {}
         self._comp = {}
 
-        # Above
+        # Create table widget
         self.table = QTableWidget()
         self.table.setColumnCount(2)
         self.table.setHorizontalHeaderLabels(["Instructions", "Compiled"])
         self.table.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
+        # Set row select
         self.table.setSelectionBehavior(QTableWidget.SelectRows)
+        # Disable editing
         self.table.setEditTriggers(QTableWidget.NoEditTriggers)
 
+        # Create text widget
         self.text = QTextEdit()
         self.text.setReadOnly(True)
 
+        # Init buttons side by side
         self.split = QHBoxLayout()
         self.split.setMargin(10)
         self.split.addWidget(self.text)
@@ -43,39 +53,42 @@ class Widget(QWidget):
         self.compile = QPushButton("Compile")
         self.clear = QPushButton("Clear")
 
+        # Set text box and table next to each other
         self.right = QHBoxLayout()
         self.right.setMargin(10)
         self.right.addWidget(self.compile)
         self.right.addWidget(self.clear)
 
-        # QWidget Layout
+        # QWidget vertical Layout
         self.layout = QVBoxLayout()
-
-        #self.table_view.setSizePolicy(size)
         self.layout.addLayout(self.split)
         self.layout.addLayout(self.right)
 
         # Set the layout to the QWidget
         self.setLayout(self.layout)
 
-        # Signals and Slots
+        # Setup signals and slots
         self.compile.clicked.connect(self.add_element)
         self.clear.clicked.connect(self.clear_table)
 
-        # Fill example data
+        # Fill table with empty data
         self.fill_table()
 
     @Slot()
     def add_element(self):
 
+        # Empty table
         self.table.setRowCount(0)
+        # Loop counter
         self.items = 0
 
+        # Compiler method
         def compile(instruction):
             #Compile C-Instruction
             if "=" in instruction or ";" in instruction and not "@" in instruction:
                 if "=" in instruction and not ";" in instruction:
                     des = instruction.split('=')
+                    # Pad with null
                     des.insert(2,'null')
                     return("111" + comp.get(des[1]) + dest.get(des[0]) + jump.get(des[2]))
                 elif "=" not in instruction and ";" in instruction:
@@ -105,6 +118,7 @@ class Widget(QWidget):
             assembly = test.splitlines()
         f.close()
 
+        # Set text box to file contents
         self.text.setPlainText(test)
 
         #Format ASM File
@@ -140,19 +154,22 @@ class Widget(QWidget):
         #Compile ASM File
         Output        = [compile(x) for x in rmw]
 
+        # Make aware of global variable
         global opfile
         opfile.clear()
 
+        # File table with assembled data
         for x, y in zip(rmw, Output):
             self.table.insertRow(self.items)
             self.table.setItem(self.items, 0, QTableWidgetItem(x))
             self.table.setItem(self.items, 1, QTableWidgetItem(y))
             self.table.resizeRowsToContents()
             self.items += 1
+            # Global list for file save
             opfile.append(y)
-            print(y)
-        
-
+            print(self.items, y)
+                
+    # Fill table method
     def fill_table(self, data=None):
         data = self._data if not data else data
         for desc, price in data.items():
@@ -164,6 +181,7 @@ class Widget(QWidget):
             self.table.setItem(self.items, 1, price_item)
             self.items += 1
 
+    # Clears table and text box
     @Slot()
     def clear_table(self):
         self.text.setPlainText(None)
@@ -174,6 +192,7 @@ class Widget(QWidget):
 class MainWindow(QMainWindow):
     def __init__(self, widget):
         QMainWindow.__init__(self)
+        # Set initial window title
         self.setWindowTitle("Hack ASM Compiler")
         
         # Menu
@@ -200,6 +219,7 @@ class MainWindow(QMainWindow):
         about_action = QAction("About", self)
         about_action.triggered.connect(self.informationMessage)
 
+        # Setup signals
         self.file_menu.addAction(open_action)
         self.file_menu.addAction(save_action)
         self.file_menu.addSeparator()
@@ -208,16 +228,18 @@ class MainWindow(QMainWindow):
         self.help_menu.addAction(about_action)
         self.setCentralWidget(widget)
 
+    # Exit program
     def exit_app(self, checked):
         QApplication.quit()
 
+    # Setup 'about' pop-up
     def informationMessage(self):
         d = QDialog()
         d.setFixedSize(240, 120)
         d.setWindowTitle("About")
         d.setWindowModality(Qt.ApplicationModal)
         d.setWindowFlag(Qt.WindowContextHelpButtonHint, False)
-
+        # Set body of pop-up windows
         d.label = QLabel("<html><head/><body><p align=\"center\">Hack ASM Compiler v0.1</p><p align=\"center\">Joseph Roddis</p><p align=\"center\">2020</p><p align=\"center\"><a href=\"github.com/SgtSiff/Nand-to-Tetris-Assembler\"><span style=\" text-decoration: underline; color:#0000ff;\">github.com/SgtSiff/Nand-to-Tetris-Assembler</span></a></p></body></html>", parent=d)
         d.label.setGeometry(QtCore.QRect(0, 0, 240, 120))
         font = QtGui.QFont()
@@ -229,18 +251,22 @@ class MainWindow(QMainWindow):
         #d.setWindowIcon(QIcon(":/icons/kdevelop.png"))
         d.exec_()
 
+    # Open file method
     def open_file(self):
         options = QFileDialog.Options()
         options |= QFileDialog.DontUseNativeDialog
         fileName, _ = QFileDialog.getOpenFileName(self,"QFileDialog.getOpenFileName()", "","ASM (*.asm)", options=options)
         if fileName:
             global openpath
+            # Store file path to global variable
             openpath = fileName
+            # Parse filename and append to window title
             filesplit = openpath.rsplit('/',1)
             windowname = "Hack ASM Compiler"
             self.setWindowTitle(windowname+" - "+filesplit[1])
             print(openpath)
 
+    # Save file method
     def save_file(self):
         options = QFileDialog.Options()
         options |= QFileDialog.DontUseNativeDialog
@@ -248,6 +274,8 @@ class MainWindow(QMainWindow):
         if fileName:
             global savepath
             savepath = fileName
+            # Fix extension to .hack
+            # Write to file
             with open(savepath+'.hack', 'w') as f:
                 for item in opfile:
                     f.write("%s\n" % item)
